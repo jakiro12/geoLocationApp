679ec4a4025f06eb57ce85da05daa28ba9bc9833
Jakiro12 <eclipsisnocte@gmail.com>
2023-07-24 21:55:13 -0300

    arreglado

--- components/DriverLocation/getCurrentDrivenLocation.jsx
+++ components/DriverLocation/getCurrentDrivenLocation.jsx
@@ -1,34 +1,33 @@
 import { useEffect, useState } from "react";
 import { View,Text } from "react-native";
 import * as Location from 'expo-location'; // paquete de ubicacion, par amostrar el ping
-import { vehiculoUpdate } from "../../Redux/Slice";
+import { vehiculoUpdate,getAllVehiculos } from "../../Redux/Slice";
 import { useDispatch, useSelector } from "react-redux";
 ////
 
 export default function GiveMeYourLocation (){
-    const [xd,setxd]=useState(true)
-
     const dispatch = useDispatch()
     const data = useSelector((state) => state.VEHICULOS.allVehiculos)
-
-  
+    const [driverLocation,setDriverLocation]=useState({
+        "identificador":"Cronm",
+        latitude:"",
+        longitude:"",}
+    )
     useEffect(()=>{
         ( async () => {
             const { status } = await Location.requestForegroundPermissionsAsync()
-            const x=0
             if (status !== 'granted') {
               console.log('Permission to access location was denied')
             } else {
               const locationSubscription = await Location.watchPositionAsync({
-                    accuracy: Location.Accuracy.Highest,
-                    timeInterval: 10000,
-                    distanceInterval: 1,
+                    accuracy: Location.Accuracy.BestForNavigation, //mejor precision para navegaciones
+                    timeInterval: 5000, // cada 5 seg
+                    distanceInterval: 15, // cada 15 metros
               }, (location) => {
-                dispatch(vehiculoUpdate({
-                  "identificador":"Cronm",
-                  "latitude":location.coords.latitude,
-                  "longitude":location.coords.longitude
-                }))
+                setDriverLocation({
+                  ...driverLocation,latitude:location.coords.latitude, longitude:location.coords.latitude
+                })
+                dispatch(vehiculoUpdate(driverLocation))
               })
             } return () => locationSubscription.remove()
           })()
@@ -41,6 +40,7 @@ export default function GiveMeYourLocation (){
         <View>
             <Text>
                 Activame la ubi perro
+            
             </Text>
         </View>
     )
